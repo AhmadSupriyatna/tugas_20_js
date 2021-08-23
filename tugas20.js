fcadcaf73ac3c3562aa5e3438335ee8f373e6261
AhmadSupriyatna <ahmadsupriyatna1@gmail.com>
2021-08-23 18:36:59 +0700

    Tugas 20 Javascript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas20.js
@@ -0,0 +1,7 @@
+function duaPuluh(){
+    var tugas = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+    console.log(tugas.match("are"));
+    console.log(tugas.match("words"));
+
+}
+duaPuluh()
